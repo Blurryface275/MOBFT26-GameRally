a89dedbc3ca43231e6c5f85028808d8ccd9eb278
Stephen <stephen.sugianto234@gmail.com>
2026-06-20 09:48:02 +0700

    chore: setup github pages deployment with next.js static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/MOBFT26-GameRally",
+  images: {
+    unoptimized: true, // Export static tidak mendukung optimasi gambar server
+  },
 };
 
 export default nextConfig;
